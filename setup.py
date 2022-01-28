9f631feb10fa190072855b1627be8c3002899a58
Sibylse <45455846+Sibylse@users.noreply.github.com>
2022-01-28 16:29:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='softmaxkmeans',
       version='0.1',
       description='Softmax k-means',
       author='Sibylle Hess',
       author_email='s.c.hess@tue.nl',
-      packages=['train','train.models','one-pixel-attack'],
+      #packages=['train','train.models','one-pixel-attack'],
+      packages=find_packages(include=["train*"]),
       install_requires=['numpy', 'scipy'],
       zip_safe=False)
