d3a052f41a457e6c302bd972834289367d0b6c4a
Sibylse <45455846+Sibylse@users.noreply.github.com>
2022-01-28 15:04:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='softmaxkmeans',
+      version='0.1',
+      description='Softmax k-means',
+      author='Sibylle Hess',
+      author_email='s.c.hess@tue.nl',
+      packages=['train','one-pixel-attack'],
+      install_requires=['numpy', 'scipy'],
+      zip_safe=False)
