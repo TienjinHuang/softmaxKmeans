214c96f263e9ea757a16573cd76d42881cf52068
Sibylse <45455846+Sibylse@users.noreply.github.com>
2022-01-28 17:13:08 +0100

    Delete setup.py

--- train/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup  
-setup(name='softmaxkmeans_models',       
-  version='0.1',       
-  description='Softmax k-means',       
-  author='Sibylle Hess',       
-  author_email='s.c.hess@tue.nl',       
-  packages=['models'],       
-  install_requires=['numpy', 'scipy'],       
-  zip_safe=False)
