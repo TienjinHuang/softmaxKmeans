69fa27a3392c87833ccbb34ee99f17ce4d939de5
Sibylse <45455846+Sibylse@users.noreply.github.com>
2022-07-19 16:14:12 +0200

    Update lenet_gauss.py

--- train/models/lenet_gauss.py
+++ train/models/lenet_gauss.py
@@ -13,17 +13,20 @@ class Gauss(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
         self.gamma=nn.Parameter(gamma*torch.ones(out_features))
-        self.Z = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
         #self.weight.requires_grad=False
         #nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
-        nn.init.uniform_(self.Z,a=0,b=0.1)
+        nn.init.uniform_(self.weight,a=0,b=0.1)
 
     def forward(self, D):
-        DX = D.mm(self.Z.t())
+        DX = D.mm(self.weight.t())
         out = torch.sum(D**2,1).unsqueeze(1).expand_as(DX)
         out = out - 2*DX
-        out = out + torch.sum(self.Z.t()**2,0).unsqueeze(0).expand_as(DX)
+        out = out + torch.sum(self.weight.t()**2,0).unsqueeze(0).expand_as(DX)
         return -F.relu(self.gamma*out)
+    
+    def conf(self,D):
+        return torch.exp(self.forward(D))
     
 class Gauss_MV(nn.Module):
     __constants__ = ['in_features', 'out_features', 'num_classes']
@@ -33,7 +36,7 @@ class Gauss_MV(nn.Module):
 
         self.in_features = in_features
         self.out_features = out_features
-        self.Z = nn.Parameter(torch.Tensor(in_features ,out_features)) #centroids (dxc)
+        self.weight = nn.Parameter(torch.Tensor(in_features ,out_features)) #centroids (dxc)
         self.W = nn.Parameter() # Whitening matrix (cxrxd)
         #self.weight.requires_grad=False
         #nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
@@ -41,13 +44,15 @@ class Gauss_MV(nn.Module):
 
     def forward(self, D):
         WDt = torch.matmul(self.W, D.t()) #c x r x m
-        WZ = torch.einsum('crd,dc->cr', self.W, self.Z) # (cxr)
+        WZ = torch.einsum('crd,dc->cr', self.W, self.weight) # (cxr)
         DMZ = torch.einsum('crj,cr->jc', WDt, WZ) # m x c
         out = torch.sum(WDt**2,1).t() # m x c
         out = out - 2*DMZ
         out = out + torch.sum(WZ**2,1).unsqueeze(0).expand_as(DMZ)
         return -F.relu(out)
 
+    def conf(self,D):
+        return torch.exp(self.forward(D))
 
 class LeNetGauss(nn.Module):
     def __init__(self,embedding_dim=84, num_classes=10, gamma=1):
@@ -56,7 +61,7 @@ class LeNetGauss(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1   = nn.Linear(16*4*4, 120)
         self.fc2   = nn.Linear(120, embedding_dim)
-        self.classifier  = Gauss(embedding_dim, num_classes,num_classes,  gamma)
+        #self.classifier  = Gauss(embedding_dim, num_classes,num_classes,  gamma)
         self.classifier  = Gauss_MV(embedding_dim, num_classes,num_classes,  gamma)
 
     def get_D(self,x):
@@ -86,7 +91,7 @@ class LeNetGauss(nn.Module):
     def get_margins(self):
         #X is dxc, out is cxc matrix, containing the distances ||X_i-X_j||
         # only the upper triangle of out is needed
-        X = self.classifier.Z.data.t()
+        X = self.classifier.weight.data.t()
         XX = X.t().mm(X)
         out = -torch.sum(X.t()**2,1).unsqueeze(1).expand_as(XX)
         out = out + 2*XX
