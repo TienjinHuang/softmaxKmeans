23db1cec10b8e0939cee0988a3d1c92831047a95
Sibylse <45455846+Sibylse@users.noreply.github.com>
2022-07-26 17:44:03 +0200

    Update optimization.py

--- train/optimization.py
+++ train/optimization.py
@@ -9,11 +9,11 @@ class Optimizer:
     
   def gradient_penalty(self, inputs, outputs):
     gradients = torch.autograd.grad(
-              outputs=outputs,
-              inputs=inputs,
-              grad_outputs=torch.ones_like(outputs),
-              create_graph=True,
-          )[0]
+            outputs=outputs,
+            inputs=inputs,
+            grad_outputs=torch.ones_like(outputs),
+            create_graph=True,
+        )[0]
     gradients = gradients.flatten(start_dim=1)
     # L2 norm
     grad_norm = gradients.norm(2, dim=1)
@@ -22,32 +22,32 @@ class Optimizer:
     return gradient_penalty
 
   def train_epoch(self, net, criterion, weight_gp_pred=0, weight_gp_embed=0):
-      #print('\nEpoch: %d' % epoch)
-      net.train()
-      train_loss, correct, total, conf, batch_idx = 0, 0, 0, 0, 0
-      for batch_idx, (inputs, targets) in enumerate(self.trainloader):
-          inputs, targets = inputs.to(self.device), targets.to(self.device)
-          self.optimizer.zero_grad()
-          inputs.requires_grad_(True)
-          embedding = net.embed(inputs)
-          loss = criterion(embedding,targets)
-          #----- gradient penalty
-          if weight_gp_pred > 0:
-              loss += weight_gp_pred * self.gradient_penalty(inputs, criterion.Y_pred)
-          if weight_gp_embed>0:
-              loss+= weight_gp_embed * self.gradient_penalty(inputs, embedding)
-          loss.backward()
-          self.optimizer.step()
-          inputs.requires_grad_(False)
+    #print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss, correct, total, conf, batch_idx = 0, 0, 0, 0, 0
+    for batch_idx, (inputs, targets) in enumerate(self.trainloader):
+      inputs, targets = inputs.to(self.device), targets.to(self.device)
+      self.optimizer.zero_grad()
+      inputs.requires_grad_(True)
+      embedding = net.embed(inputs)
+      loss = criterion(embedding,targets)
+      #----- gradient penalty
+      if weight_gp_pred > 0:
+        loss += weight_gp_pred * self.gradient_penalty(inputs, criterion.Y_pred)
+      if weight_gp_embed>0:
+        loss+= weight_gp_embed * self.gradient_penalty(inputs, embedding)
+      loss.backward()
+      self.optimizer.step()
+      inputs.requires_grad_(False)
 
-          with torch.no_grad():
-              if self.update_centroids:
-                  net.eval()
-                  criterion.classifier.update_centroids(embedding, criterion.Y)
-              train_loss += loss.item()
-              confBatch, predicted = criterion.conf(embedding).max(1)
-              total += targets.size(0)
-              correct += predicted.eq(targets).sum().item()
-              conf+=confBatch.sum().item()
-        print('Loss: %.3f | Acc: %.3f%% (%d/%d) | Conf %.2f'% (100*train_loss/batch_idx, 100.*correct/total, correct, total, 100*conf/total))
-        return 100.*correct/total, 100*conf/total
+      with torch.no_grad():
+        if self.update_centroids:
+          net.eval()
+          criterion.classifier.update_centroids(embedding, criterion.Y)
+        train_loss += loss.item()
+        confBatch, predicted = criterion.conf(embedding).max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+        conf+=confBatch.sum().item()
+    print('Loss: %.3f | Acc: %.3f%% (%d/%d) | Conf %.2f'% (100*train_loss/batch_idx, 100.*correct/total, correct, total, 100*conf/total))
+    return (100.*correct/total, 100*conf/total)
