28a023a796b73552363627711e3c47519b406d55
Sibylse <45455846+Sibylse@users.noreply.github.com>
2022-07-29 23:15:13 +0200

    Update vgg.py

--- train/models/vgg.py
+++ train/models/vgg.py
@@ -14,33 +14,15 @@ cfg = {
     'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
 }
 
-class VGG(nn.Module):
-    def __init__(self, vgg_name, gc=0):
+class VGGEmbed(nn.Module):
+    def __init__(self, vgg_name):
         super(VGG, self).__init__()
         self.features = self._make_layers(cfg[vgg_name])
-        if gc:
-            #self.classifier = LinearKM(512, 10)
-            classes = 11
-        else:
-            classes = 10
-        if vgg_name == 'VGG16_100':
-            self.classifier = nn.Linear(100,classes,bias=False)
-        elif vgg_name == 'VGG16_2':
-            self.classifier = nn.Linear(2,classes,bias=False)
-        elif vgg_name == 'VGG16_10':
-            self.classifier = nn.Linear(10,classes,bias=False)
-        else :
-            self.classifier = nn.Linear(512,classes,bias=False)
 
     def forward(self, x):
         out = self.features(x)
         out = out.view(out.size(0), -1)
-        out = self.classifier(out)
         return out
-
-    def get_D(self,x):
-        out = self.features(x)
-        return out.view(out.size(0), -1)
 
     def _make_layers(self, cfg):
         layers = []
@@ -55,12 +37,15 @@ class VGG(nn.Module):
                 in_channels = x
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
         return nn.Sequential(*layers)
+    
+class VGG(nn.Module):
+    def __init__(self, vgg_name, classifier):
+        super(VGG, self).__init__()
+        self.embed = VGGEmbed(vgg_name)
+        #self.classifier = nn.Linear(512,classes,bias=True)
+        self.classifier = classifier
 
-
-def test():
-    net = VGG('VGG11')
-    x = torch.randn(2,3,32,32)
-    y = net(x)
-    print(y.size())
-
-# test()
+    def forward(self, x):
+        out = self.embed(x)
+        out = self.classifier(out)
+        return out
