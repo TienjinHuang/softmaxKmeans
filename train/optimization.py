967fa796906c7c51fbfec42ceb2b3a3eb4908b98
Sibylse <45455846+Sibylse@users.noreply.github.com>
2022-08-09 18:12:49 +0200

    Update optimization.py

--- train/optimization.py
+++ train/optimization.py
@@ -80,7 +80,7 @@ class Optimizer:
         for batch_idx, (inputs, targets) in enumerate(self.trainloader):
             inputs, targets = inputs.to(self.device), targets.to(self.device)
             D = net.embed(inputs)
-            Y = F.one_hot(targets, c).float()
+            Y = F.one_hot(targets, c).float().to(device)
             Z += D.t().mm(Y)
             y_sum += torch.sum(Y,0)
     Z = Z/y_sum
